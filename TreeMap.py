0bfe812acb89f44a8fc66281b078f346b1268bbe
Oliver Powell <mail@oliverpowell.com>
2023-07-25 14:39:18 +0100

    added logging to TreeMap

--- TreeMap.py
+++ TreeMap.py
@@ -1,14 +1,24 @@
 import plotly.express as px
 import pandas as pd
 import click
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
+                    level=logging.INFO)
 
 @click.command()
-@click.argument('excel file')
+@click.argument('excel_file')
 def main(excel_file):
     """
     Tree Map is a simple tool that shows the positions of previously identified trees as a coloured dots on a map using GPS coordinates. The colour of the dot is determined by the score assigned to a given tree ranging from 0 to 10. See [GITHUB LINK] for full documentation.
+    
+    TreeMap was delevoped by Oliver Powell on 25/07/23. For more info please contact: treemap@oliverpowell.com 
     """
+    logging.info("Loading data...")
     df = pd.read_excel(excel_file)
+
+    logging.info("Analysing data...")
     df['Average Score'] = round(df.groupby('Tree')['Score (0-10)'].transform('mean'))
     Se = pd.DataFrame(df.groupby('Tree')['Score (0-10)'].agg(list))
     Se.columns = ['Score List']
@@ -16,7 +26,7 @@ def main(excel_file):
     colour_scale = []
 
 
-
+    logging.info("Plotting trees...")
     fig = px.scatter_mapbox(df, 
                             lat="North", 
                             lon="East", 
